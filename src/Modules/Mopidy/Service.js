784bf255dd8f8ab2628fcff74af04143a845afcc
Clancy Whittle <cwhittle@agl.com.au>
2017-01-26 16:21:02 +1300

    play, pause, resume, seek updates received and working.

--- src/Modules/Mopidy/Service.js
+++ src/Modules/Mopidy/Service.js
@@ -1,5 +1,6 @@
-import Mopidy from 'mopidy'
-
+import Mopidy from 'mopidy';
+import store from '../../redux/store';
+import * as MopidyActions from './Actions';
 var mopidy = null;
 
 export function connect(webSocketUrl) {
@@ -13,9 +14,47 @@ export function connect(webSocketUrl) {
     mopidy.on("state:online", function () {
         resolve();
     });
+
+    mopidy.on(eventSupervisor);
+
     mopidy.connect();
   });
 };
+
+function eventSupervisor(event, data){
+  console.log(JSON.stringify(event));
+  var cache = [];
+  var objString = JSON.stringify(data, function(key, value) {
+      if (typeof value === 'object' && value !== null) {
+          if (cache.indexOf(value) !== -1) {
+              // Circular reference found, discard key
+              return;
+          }
+          // Store value in our collection
+          cache.push(value);
+      }
+      return value;
+  });
+  cache = null;
+  console.log(JSON.stringify(objString));
+  switch(event){
+    case "event:trackPlaybackPaused":
+        store.dispatch(MopidyActions.receiveTrackPosition(data.time_position));
+        store.dispatch(MopidyActions.receivePlaybackStatus("paused"));
+      break;
+    case "event:trackPlaybackResumed":
+        store.dispatch(MopidyActions.receiveTrackPosition(data.time_position));
+        store.dispatch(MopidyActions.receivePlaybackStatus("playing"));
+      break;
+    case "event:trackPlaybackStarted":
+        store.dispatch(MopidyActions.receiveTrackPosition(0));
+        store.dispatch(MopidyActions.receiveCurrentTrack(data.tl_track.track));
+      break;
+    case "event:seeked":
+        store.dispatch(MopidyActions.receiveTrackPosition(data.time_position));
+      break;
+  }
+}
 
 export function getCurrentTrack() {
   var a = mopidy.playback.getCurrentTrack();
